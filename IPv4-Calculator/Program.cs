08faac02d077b5b30f9c881f44ab495dcad68da3
gabarg <gabarg@F-GABARG-NB>
2025-11-24 11:14:19 +0100

    Add project files.

---
+++ IPv4-Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace IPv4_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("IP-Address: ");
+            var ipAddress = Console.ReadLine();
+            Console.Write("Subnetmask: ");
+            var snm = Console.ReadLine();
+            
+            var IpCalc = new BitIpAddressCalc(ipAddress, snm);
+            Console.WriteLine(Environment.NewLine + IpCalc.ToString());
+        }
+    }
+}
